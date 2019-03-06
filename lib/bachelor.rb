c5e6b65568e4034a3b7b68486d740f4df01aa0bb
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-06 15:03:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,7 @@ def get_first_name_of_season_winner(data, season)
       info.each do |girl|
         girl.each do |key, value|
           
-          if value == "winner"
+          if key == "status" && value == "winner"
             binding.pry
             print full_name = girl["name"]
             
